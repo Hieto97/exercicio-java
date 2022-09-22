b7aebd50e5318eee739dab76656ab3f2608d35cb
Hieto97 <Joaosalomao987@gmail.com>
2022-09-22 19:03:21 -0300

    teste para o github

--- Exercicio2.java
+++ Exercicio2.java
@@ -1,5 +1,5 @@
 public class Exercicio2 {
     public static void main(String[] args) {
-        System.out.println(" Hello world ");
+        System.out.println(" teste para usar o github ");
     }
 }
