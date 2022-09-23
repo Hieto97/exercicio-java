a756a07e55bf33def2a27b1cbb620610f3eb98da
Hieto97 <Joaosalomao987@gmail.com>
2022-09-22 21:27:09 -0300

    novos exercicios praticados matriz e veetor

--- Exercicio2.java
+++ Exercicio2.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercicio2 {
     public static void main(String[] args) {
-        System.out.println(" teste para usar o github ");
+        //Vetor
+        String[] listadeInformacoes = new String[5];
+        listadeInformacoes[0] = " Detergente ";
+        listadeInformacoes[1] = " chocolate" ;
+        listadeInformacoes[2] = " sucrilhos" ;
+        listadeInformacoes[3] = "arroz";
+        listadeInformacoes[4] = "bacon";
+
+        System.out.println(listadeInformacoes[4]);
+
+        //Matriz
+
+        List<String> listadeInformacoesdenumeroInteiro = new ArrayList<>();
+
+        listadeInformacoesdenumeroInteiro.add("carteira");
+        listadeInformacoesdenumeroInteiro.add("bone");
+        listadeInformacoesdenumeroInteiro.add("lapis");
+        listadeInformacoesdenumeroInteiro.add("mouse");
+        listadeInformacoesdenumeroInteiro.add("oculos");
+        listadeInformacoesdenumeroInteiro.add("lente");
+
+        System.out.println(listadeInformacoesdenumeroInteiro.contains("lente"));
     }
 }
