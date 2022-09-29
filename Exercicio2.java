0bf9878150d6bf19250f122f29b386458dab5631
Hieto97 <Joaosalomao987@gmail.com>
2022-09-29 18:39:36 -0300

    listadeInformacoes

--- Exercicio2.java
+++ Exercicio2.java
@@ -4,26 +4,57 @@ import java.util.List;
 public class Exercicio2 {
     public static void main(String[] args) {
         //Vetor
-        String[] listadeInformacoes = new String[5];
-        listadeInformacoes[0] = " Detergente ";
-        listadeInformacoes[1] = " chocolate" ;
-        listadeInformacoes[2] = " sucrilhos" ;
-        listadeInformacoes[3] = "arroz";
-        listadeInformacoes[4] = "bacon";
-
-        System.out.println(listadeInformacoes[4]);
+//        String[] listadeInformacoes = new String[5];
+//        listadeInformacoes[0] = " Detergente ";
+//        listadeInformacoes[1] = " chocolate" ;
+//        listadeInformacoes[2] = " sucrilhos" ;
+//        listadeInformacoes[3] = "arroz";
+//        listadeInformacoes[4] = "bacon";
+//
+//        System.out.println(listadeInformacoes[4]);
 
         //Matriz
 
-        List<String> listadeInformacoesdenumeroInteiro = new ArrayList<>();
+//        List<String> listadeInformacoesdenumeroInteiro = new ArrayList<>();
+//
+//        listadeInformacoesdenumeroInteiro.add("carteira");
+//        listadeInformacoesdenumeroInteiro.add("bone");
+//        listadeInformacoesdenumeroInteiro.add("lapis");
+//        listadeInformacoesdenumeroInteiro.add("mouse");
+//        listadeInformacoesdenumeroInteiro.add("oculos");
+//        listadeInformacoesdenumeroInteiro.add("lente");
+//
+//        System.out.println(listadeInformacoesdenumeroInteiro.contains("lente"));
 
-        listadeInformacoesdenumeroInteiro.add("carteira");
-        listadeInformacoesdenumeroInteiro.add("bone");
-        listadeInformacoesdenumeroInteiro.add("lapis");
-        listadeInformacoesdenumeroInteiro.add("mouse");
-        listadeInformacoesdenumeroInteiro.add("oculos");
-        listadeInformacoesdenumeroInteiro.add("lente");
+//Exercicio 1
 
-        System.out.println(listadeInformacoesdenumeroInteiro.contains("lente"));
+//   String[] PersonagensDesbloqueados = new String[20];
+//        PersonagensDesbloqueados[0] = "Jason";
+//        PersonagensDesbloqueados[1] = "Viego";
+//        PersonagensDesbloqueados[2] = "Jinx";
+//        PersonagensDesbloqueados[3] = "Arthur Morgan";
+//        PersonagensDesbloqueados[4] = "Morgana";
+//        PersonagensDesbloqueados[5] = "Cloud";
+//        PersonagensDesbloqueados[6] = "Vladimir";
+//        PersonagensDesbloqueados[7] = "Hieto";
+//        PersonagensDesbloqueados[8] = "Solid Snake";
+//        PersonagensDesbloqueados[9] = "Joker";
+//        PersonagensDesbloqueados[10] = "Michael";
+//        PersonagensDesbloqueados[11] = "Pinhead";
+//        PersonagensDesbloqueados[12] = "Xenomorph";
+//        PersonagensDesbloqueados[13] = "Predador";
+//        PersonagensDesbloqueados[14] = "Dracula";
+//        PersonagensDesbloqueados[15] = "Alucard";
+//        PersonagensDesbloqueados[16] = "Leon";
+//        PersonagensDesbloqueados[17] = "Claire";
+//        PersonagensDesbloqueados[18] = "Nemesis";
+//        PersonagensDesbloqueados[19] = "John Marstom";
+//
+//           System.out.println(PersonagensDesbloqueados[1] + " , " + PersonagensDesbloqueados[14] + " , " + PersonagensDesbloqueados[3] + " , " + PersonagensDesbloqueados[6]
+//                    + " , " + PersonagensDesbloqueados[9] + " , " + PersonagensDesbloqueados[8] + " , " + PersonagensDesbloqueados[12] + " , " + PersonagensDesbloqueados[2] + " , " + PersonagensDesbloqueados[15]
+//                    + " , " + PersonagensDesbloqueados [16]);
+
+  //Exercicio2
+
     }
 }
